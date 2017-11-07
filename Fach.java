78995416d5717fb486ba810542b0a71c51bfbcd4
Tobias Witte <wittetobias@web.de>
2017-11-07 09:13:08 +0100

    Hallo 3

--- Fach.java
+++ Fach.java
@@ -1,71 +1,75 @@
 /**
-*
-*/
+ *
+ */
 public class Fach 
 {
-  /* Attribute */
-	private final String name;
-	private int notenziel;
-	private Note[] note;
-  /* Konstruktoren */
-  /**
-* Konstruktor für die Klasse Fach
-* @param Name
-*/
-  public Fach(String  name){
-      this.name = name;
-  }
+    /* Attribute */
+    private final String name;
+    private int notenziel;
+    private Note[] note;
+    /* Konstruktoren */
+    /**
+     * Konstruktor für die Klasse Fach
+     * @param Name
+     */
+    public Fach(String  name){
+        this.name = name;
+    }
 
-  /* Methoden */
-  /** @return liefert den Notendurchschnitt
-*/
-  public int gibNotendurchschnitt(){
-      return 0;
-  }
+    /* Methoden */
+    /** @return liefert den Notendurchschnitt
+     */
+    public int gibNotendurchschnitt(){
+        int sum = 0;
+        for(int i=0; i<note.length; i++){
+            sum += note[i].gibNote();
+        }
+        return sum/note.length;
+    }
 
-  /** @return liefert die aktuelle Tendenz zurück
-*/
-  public int gibAktuelleTendenz(){
-      return 0;
-  }
+    /** @return liefert die aktuelle Tendenz zurück
+     */
+    public int gibAktuelleTendenz(){
+        return 0;
+    }
 
-  /** @param schriftliche Note 
-*/
-  public void legeSchriftlicheNoteAn(int  note){
-  }
+    /** @param schriftliche Note 
+     */
+    public void legeSchriftlicheNoteAn(int  note){
+    }
 
-  /** @return liefert name 
-*/
-  public String gibName(){
-    //TODO gegebenenfalls ueberarbeiten
-    return this.name;
-  }
+    /** @return liefert name 
+     */
+    public String gibName(){
+        //TODO gegebenenfalls ueberarbeiten
+        return this.name;
+    }
 
-  /** @return liefert notenziel 
-*/
-  public int gibNotenziel(){
-    //TODO gegebenenfalls ueberarbeiten
-    return this.notenziel;
-  }
+    /** @return liefert notenziel 
+     */
+    public int gibNotenziel(){
+        //TODO gegebenenfalls ueberarbeiten
+        return this.notenziel;
+    }
 
-  /** setzt notenziel 
-*/
-  public void setzeNotenziel(int  pNotenziel){
-    //TODO gegebenenfalls ueberarbeiten
-    this.notenziel = pNotenziel;
-  }
+    /** setzt notenziel 
+     */
+    public void setzeNotenziel(int  pNotenziel){
+        //TODO gegebenenfalls ueberarbeiten
+        this.notenziel = pNotenziel;
+    }
 
-  /** @return liefert die Differenz zwischen 
-* Notenziel und aktuellem Notendurchschnitt
-*/
-  public int gibDifferenz(){
-      return 0;
-  }
+    /** @return liefert die Differenz zwischen 
+     * Notenziel und aktuellem Notendurchschnitt
+     */
+    public int gibDifferenz(){
+        return 0;
+    }
 
-  /** @param  muendliche Note
-*/
-  public void legeMuendlicheNoteAn(int  note){
-  }
+    /** @param  muendliche Note
+     */
+    public void legeMuendlicheNoteAn(int  note){
+    }
 
 }//Ende Klasse: Fach
 
